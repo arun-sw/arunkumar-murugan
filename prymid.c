9b90637bd9f1ae7e3e667011335f87cd4fa9e1f2
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-03 19:55:01 +0530

    Add files via upload

---
+++ prymid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main() {
+    int n, i, s, k = 0;
+    printf("num:");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++, k = 0) {
+        for (s = 1; s <= n - i; s++) {
+
+            printf("  ");
+
+        }
+        while (k != 2 * i - 1) {
+            printf(" %d",i);
+            k++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
