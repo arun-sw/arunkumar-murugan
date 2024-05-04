3fb0b084776ec1f0ae2290a62ec9a69cec6607a1
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-04 20:30:50 +0530

    Add files via upload

---
+++ factorial-r.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+
+int fact(int n)
+{
+    if(n==1)
+
+        return 1;
+    return n*fact(n-1);
+}
+int main()
+    {
+    int n,sum;
+    printf("enter the num:");
+    scanf("%d",&n);
+
+    sum=fact(n);
+        printf(" fact is %d=%d",n,sum);
+        return 0;
+    }
