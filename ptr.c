b791d2d6bb06326fe5390fcfc901409bdad08c55
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-08 19:03:53 +0530

    Add files via upload

---
+++ ptr.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main(char argv[],char argc[])
+{
+   FILE *ptr1,*ptr2;
+   char ch;
+   int letter=0,words=0,line=0;
+
+   ptr1= fopen(argc[1],"r");
+   if(ptr1==NULL)
+   {
+       printf("file is not opened");
+   }
+    ptr2= fopen("arun","w");
+    if(ptr2==NULL)
+    {
+        printf("file is not opened");
+    }
+    while (ch= fgetc(ptr1)!=EOF){
+
+        fputc(ch,ptr2);
+        letter++;
+        if(ch=='\n'||ch=='\0'||ch=='\t'){
+            line++;
+        }
+        if(ch=='\n'||ch=='\0'||ch=='\t'||ch==' ') {
+            words++;
+        }
+        if (letter<0){
+            words++;
+            line++;
+        }
+
+        fprintf(ptr2,"NO OF WORDS");
+        fprintf(ptr2,"%d",words);
+
+        fprintf(ptr2,"NO OF line");
+        fprintf(ptr2,"%d",line);
+
+        fprintf(ptr2,"NO OF letter");
+        fprintf(ptr2,"%d",letter);
+
+        printf("file is copyed succfully");
+        fclose(ptr1);
+        fclose(ptr2);
+        return 0;
+    }
+
+}
