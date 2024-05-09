2dc3016bd1ecaebc89ef2eae33c56d3be9aea6ec
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-09 21:24:27 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -6,25 +6,25 @@ int main() {
     printf("enter the number");
     scanf("%d %d", &a, &n);
     int i, j;
-    int mat1[m][n], mat2[m][n], mat3[m][n];
+    int matrix1[m][n], matrix[m][n], matrix3[m][n];
     for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++)
             scanf("%d", &matrix1[i][j]);
     }
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++)
-            scanf("%d", &mat2[i][j]);
+            scanf("%d", &matrix2[i][j]);
     }
 
     for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++) {
-            mat3[i][j] = mat1[i][j] + mat2[i][j];
+            matrix3[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
 
     for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++)
-            printf("%d", mat3[i][j]);
+            printf("%d", matrix3[i][j]);
         printf("\n");
     }
 
