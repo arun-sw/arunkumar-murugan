f7b652a1c5d7e3ef3efba7f9ffacb08d2d1fd4ba
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-09 21:20:33 +0530

    Update matrix.c

--- matrix.c
+++ matrix.c
@@ -2,12 +2,12 @@
 #include <conio.h>
 
 int main() {
-    int m, n;
+    int a, n;
     printf("enter no");
-    scanf("%d %d", &m, &n);
+    scanf("%d %d", &a, &n);
     int i, j;
     int mat1[m][n], mat2[m][n], mat3[m][n];
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++)
             scanf("%d", &mat1[i][j]);
     }
@@ -16,13 +16,13 @@ int main() {
             scanf("%d", &mat2[i][j]);
     }
 
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++) {
             mat3[i][j] = mat1[i][j] + mat2[i][j];
         }
     }
 
-    for (i = 0; i < m; i++) {
+    for (i = 0; i < a; i++) {
         for (j = 0; j < n; j++)
             printf("%d", mat3[i][j]);
         printf("\n");
