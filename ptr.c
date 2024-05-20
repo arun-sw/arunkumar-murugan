f6f104f0125554b248c280a0e59eb1abd135f6fd
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-05-20 19:06:35 +0530

    Add files via upload

--- ptr.c
+++ ptr.c
@@ -2,47 +2,47 @@
 
 int main(char argv[],char argc[])
 {
-   FILE *p1,*p2;
+   FILE *ptr1,*ptr2;
    char ch;
-   int character=0,words=0,line=0;
+   int letter=0,words=0,line=0;
 
-   p1= fopen(argc[1],"r");
-   if(p1==NULL)
+   ptr1= fopen(argc[1],"r");
+   if(ptr1==NULL)
    {
        printf("file is not opened");
    }
-    p2= fopen("arun","w");
-    if(p2==NULL)
+    ptr2= fopen("arun","w");
+    if(ptr2==NULL)
     {
         printf("file is not opened");
     }
-    while (ch= fgetc(p1)!=EOF){
+    while (ch= fgetc(ptr1)!=EOF){
 
-        fputc(ch,p2);
-        charcter++;
+        fputc(ch,ptr2);
+        letter++;
         if(ch=='\n'||ch=='\0'||ch=='\t'){
             line++;
         }
         if(ch=='\n'||ch=='\0'||ch=='\t'||ch==' ') {
             words++;
         }
-        if (character<0){
+        if (letter<0){
             words++;
             line++;
         }
 
-        fprintf(p2,"NO OF WORDS");
-        fprintf(p2,"%d",words);
+        fprintf(ptr2,"NO OF WORDS");
+        fprintf(ptr2,"%d",words);
 
-        fprintf(p2,"NO OF line");
-        fprintf(p2,"%d",line);
+        fprintf(ptr2,"NO OF line");
+        fprintf(ptr2,"%d",line);
 
-        fprintf(p2,"NO OF letter");
-        fprintf(p2,"%d",letter);
+        fprintf(ptr2,"NO OF letter");
+        fprintf(ptr2,"%d",letter);
 
         printf("file is copyed succfully");
-        fclose(p1);
-        fclose(p2);
+        fclose(ptr1);
+        fclose(ptr2);
         return 0;
     }
 
