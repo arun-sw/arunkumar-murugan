1fbe3c43c3509b90199b22ba4f00f47bcb587d15
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-17 21:56:52 +0530

    Update flot-mul.c

--- flot-mul.c
+++ flot-mul.c
@@ -5,10 +5,10 @@ int main()
     float num1,num2;
      float sum;
      printf("enter the value");
-     scanf("%f",&a);
+     scanf("%f",&num1);
      
      printf("");
-     scanf("%f",&b);
+     scanf("%f",&num2);
      
      sum=a*b;
      
