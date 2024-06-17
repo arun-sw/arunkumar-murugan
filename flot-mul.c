5e2a08f4708d606d95282df527b6eb4dfbd1722d
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-17 21:55:15 +0530

    Add files via upload

---
+++ flot-mul.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    float a,b;
+     float sum;
+     printf("enter the value");
+     scanf("%f",&a);
+     
+     printf("");
+     scanf("%f",&b);
+     
+     sum=a*b;
+     
+     printf("sum ;%f",sum);
+    return 0;
+}
