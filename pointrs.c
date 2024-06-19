41c57051bdccaf366104e8916a3504777d0adec0
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-19 21:30:26 +0530

    Add files via upload

--- pointrs.c
+++ pointrs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <conio.h>
 #include <math.h>
 #include <stdlib.h>
 struct student
@@ -15,24 +16,24 @@ int main()
 struct student s[2];
 int i,avg,total=0, marks[5];
     printf("enter the student roll no:");
-    scanf("%d",&s.rollno);
+    scanf("%d",&s[2].rollno);
     printf("enter the student name:");
-    scanf("%s",&s.name);
+    scanf("%s",&s[2].name);
     printf("enter the student mark\n");
     printf("enter the sub %d:",i+1);
-    scanf("%d",&s.sub1);
+    scanf("%d",&s[2].sub1);
     printf("enter the sub %d:",i+1);
-    scanf("%d",&s.sub2);
+    scanf("%d",&s[2].sub2);
     printf("enter the sub %d:",i+1);
-    scanf("%d",&s.sub3);
+    scanf("%d",&s[2].sub3);
     printf("enter the sub %d:",i+1);
     scanf("%d",&s[2].sub4);
     printf("enter the sub %d:",i+1);
-    scanf("%d",&s.sub5);
+    scanf("%d",&s[2].sub5);
     printf("\tSTUTENT MARKSHEET\n");
-    printf("student roll no:%d\n",s.rollno);
-    printf("student name :%s\n",s.name);
-    total=s.sub1+s.sub2+s.sub3+s.sub4+s.sub5;
+    printf("student roll no:%d\n",s[2].rollno);
+    printf("student name :%s\n",s[2].name);
+    total=s[2].sub1+s[2].sub2+s[2].sub3+s[2].sub4+s[2].sub5;
     printf("total marks %d\n",total);
     avg=total/5;
     printf("avg mark:%d\n",avg);
