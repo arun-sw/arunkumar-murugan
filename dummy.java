36ba322a6ae2e9455a54bede3242a15e7251d6d3
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-21 20:19:51 +0530

    Add files via upload

---
+++ dummy.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+public class dummy {
+    public static <date> void main(String[] args) {
+        int a=5;
+        int b=10;
+         int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        float f=(float) a/(float) b;
+        int g= a%b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+        
+    }
+    }
+
