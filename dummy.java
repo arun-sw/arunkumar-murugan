88e3fdc9eb86cb16669baabcaddf1f0f2ad9c79c
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-21 20:21:25 +0530

    Update dummy.java

--- dummy.java
+++ dummy.java
@@ -1,12 +1,12 @@
-import java.util.Date;
+import java. util.Date;
 public class dummy {
     public static <date> void main(String[] args) {
         int a=5;
         int b=10;
          int c=a+b;
-        int d=a-b;
-        int e=a*b;
-        float f=(float) a/(float) b;
+        int r=a-b;
+        int k=a*b;
+        float l=(float) a/(float) b;
         int g= a%b;
 
         System.out.println(c);
