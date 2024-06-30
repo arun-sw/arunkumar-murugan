379b0ce83be1693ffbabc7208fa86e3ba5ac5b22
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-06-30 19:01:20 +0530

    Add files via upload

---
+++ pallidorme-no.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <conio.h>
+
+int main()
+{
+int n,rev=0;
+int copy;
+    printf ("enter the n:");
+    scanf("%d",&n);
+    copy =n;
+while(copy>0)
+      {  
+       rev=rev*10;
+       rev=rev +(copy%10);
+       copy=copy/10;
+       }
+if(n==rev)
+
+      {
+       printf("ama");
+       }
+else  
+      {
+       printf("illa");
+      }
+return 0;
+}
