f27dd407511b979c912c841789e46de5d041fcc6
Arunkumar murungan <154626042+arun-sw@users.noreply.github.com>
2024-07-29 22:16:39 +0530

    Update dummy.java

--- dummy.java
+++ dummy.java
@@ -1,12 +1,11 @@
-
 public class simple {
     public static  void main(String[] args) {
         int a=60;
         int b=40;
          int e=a+b;
         int r=a-b;
-        int k=a*b;
-        float l=(float) a/(float) b;
+        int  k= a*b;
+        float  l=(float) a/(float) b;
         int h= a%b;
 
         System.out.println(e);
